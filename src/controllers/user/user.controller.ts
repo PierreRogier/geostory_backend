ab76ed22abb5420e3eb702754802f499bf5c6edb
PierreRogier <pierre.rogier3@gmail.com>
2022-12-08 14:13:03 +0100

    Create User Controller

--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
+import { UserService } from '@services';
+import { UpdateUserDto } from '@core/dtos';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get('/')
+  findAllUsers() {
+    return this.userService.findAllUsers();
+  }
+
+  @Get(':userId')
+  findUserById(@Param('userId') userId: string) {
+    return this.userService.findUserById(+userId);
+  }
+
+  @Patch(':userId')
+  updateUser(
+    @Param('userId') userId: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return this.userService.updateUser(+userId, updateUserDto);
+  }
+
+  @Delete(':userId')
+  remove(@Param('userId') userId: string) {
+    return this.userService.removeUser(+userId);
+  }
+}
